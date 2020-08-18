70103ec48191f606aeb71f1a3920ce7ccc8e91f7
PythonNut <PythonNut@users.noreply.github.com>
2020-08-17 21:17:12 -0700

    Disable use of auth-info for TRAMP

--- modules/config-tramp.el
+++ modules/config-tramp.el
@@ -23,6 +23,7 @@
 
 (with-eval-after-load 'tramp
   (eval-when-compile (require 'tramp))
+  (setq tramp-completion-use-auth-sources nil)
   ;; Define a rsyncx method analogous to scpx
   (add-to-list 'tramp-methods
                `("rsyncx"
